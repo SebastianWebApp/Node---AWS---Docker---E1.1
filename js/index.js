0f77b4700986b5adcb2462ed6ae4d0646582ad4d
Mateo Espinosa <94413681+SebastianWebApp@users.noreply.github.com>
2024-12-22 18:18:23 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.getElementById('fetchUsers').addEventListener('click', async () => {
     try {
-      const response = await fetch('http://localhost:4000/api/usuarios');
+      const response = await fetch('http://ec2-3-87-1-100.compute-1.amazonaws.com:4000/api/usuarios');
       if (!response.ok) throw new Error('Error al obtener usuarios');
       
       const users = await response.json();
